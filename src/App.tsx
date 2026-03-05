f85d8afddf666a46f22587fbfc8d261596351c0b
Gugzz21 <gustavodinizz966@gmail.com>
2026-03-05 16:41:13 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,16 @@ import Juros from './components/Juros'
 
 function App() {
   return (
-    // 1. Colocamos o bg-gray-100 e min-h-screen na div MAIS externa.
-    // Agora o fundo da página inteira é cinza e ocupa a tela toda.
     <div className="flex flex-col items-center bg-gray-100 min-h-screen w-full">
 
-      {/* O Header pode ter a cor que você quiser lá dentro do componente dele */}
       <Header />
 
-      {/* 2. Removemos o bg-gray-100 daqui, pois o fundo de trás já é cinza.
-             Mantivemos o padding para posicionar o seu texto. */}
       <div className="flex flex-col w-full pl-[350px] pt-[15px] pb-[15px]">
         <p className="text-[14px]">
           Todos os valores estão em Reais (R$) A presente calculadora virtual não substitui o cálculo realizado e homologado judicialmente.
         </p>
       </div>
 
-      {/* 3. Aqui ficarão os seus componentes. Eles podem ter seus próprios fundos (como o bg-amber-100 do seu Form) */}
       <div className="flex flex-col justify-center items-center mt-10 gap-4">
         <Form />
         <Data />
